c88acd3c67e50dad8832426a4206555579ee85bc
ryanycw <ryan@ryanycw.dev>
2025-06-23 23:38:28 +0800

    Refactor main.rs to comment out database connection logic and logging setup, and add persistence module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+// #[macro_use]
+// extern crate log;
+// extern crate pretty_env_logger;
+
 use dotenvy::dotenv;
-use log::info;
+//use log::info;
 use pretty_env_logger;
-use sqlx::postgres::PgPoolOptions;
+//use sqlx::postgres::PgPoolOptions;
 
 use axum::{
     Router,
@@ -10,6 +14,7 @@ use axum::{
 
 mod handlers;
 mod models;
+mod persistance;
 
 use handlers::*;
 
@@ -18,27 +23,11 @@ async fn main() {
     pretty_env_logger::init();
     dotenv().ok();
 
-    // Create a new PgPoolOptions instance with a maximum of 5 connections.
-    // Use dotenv to get the database url.
-    // Use the `unwrap` or `expect` method instead of handling errors. If an
-    // error occurs at this stage the server should be terminated.
-    // See examples on GitHub page: https://github.com/launchbadge/sqlx
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set"))
-        .await
-        .expect("Failed to connect to database");
-
-    // Using slqx, execute a SQL query that selects all questions from the questions table.
-    // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
-    // make sure we can connect to the database.
-    let recs = sqlx::query!("SELECT * FROM questions")
-        .fetch_all(&pool)
-        .await
-        .expect("Failed to fetch questions");
-
-    info!("********* Question Records *********");
-    info!("{:?}", recs);
+    // let pool = PgPoolOptions::new()
+    //     .max_connections(5)
+    //     .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set"))
+    //     .await
+    //     .expect("Failed to connect to database");
 
     let app = Router::new()
         .route("/question", post(create_question))
