e7cdb7a78763a3c2dfb844b9d8a5bade76c96d88
ryanycw <ryan@ryanycw.dev>
2025-06-23 16:25:20 +0800

    Implement main application logic with database connection and routing for questions and answers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::dotenv;
+use log::info;
+use pretty_env_logger;
+use sqlx::postgres::PgPoolOptions;
+
+use axum::{
+    Router,
+    routing::{delete, get, post},
+};
+
+mod handlers;
+mod models;
+
+use handlers::*;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    dotenv().ok();
+
+    // Create a new PgPoolOptions instance with a maximum of 5 connections.
+    // Use dotenv to get the database url.
+    // Use the `unwrap` or `expect` method instead of handling errors. If an
+    // error occurs at this stage the server should be terminated.
+    // See examples on GitHub page: https://github.com/launchbadge/sqlx
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set"))
+        .await
+        .expect("Failed to connect to database");
+
+    // Using slqx, execute a SQL query that selects all questions from the questions table.
+    // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
+    // make sure we can connect to the database.
+    let recs = sqlx::query!("SELECT * FROM questions")
+        .fetch_all(&pool)
+        .await
+        .expect("Failed to fetch questions");
+
+    info!("********* Question Records *********");
+    info!("{:?}", recs);
+
+    let app = Router::new()
+        .route("/question", post(create_question))
+        .route("/questions", get(read_questions))
+        .route("/question", delete(delete_question))
+        .route("/answer", post(create_answer))
+        .route("/answers", get(read_answers))
+        .route("/answer", delete(delete_answer));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
