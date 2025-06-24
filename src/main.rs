7d42e6cb8b84fca817e07c4714237dda630c422c
ryanycw <ryan@ryanycw.dev>
2025-06-24 20:27:13 +0800

    Refactor main.rs to restore database connection logic and initialize application state with DAO implementations for questions and answers

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-// #[macro_use]
-// extern crate log;
-// extern crate pretty_env_logger;
-
-use dotenvy::dotenv;
-//use log::info;
-use pretty_env_logger;
-//use sqlx::postgres::PgPoolOptions;
+use std::sync::Arc;
 
 use axum::{
     Router,
     routing::{delete, get, post},
+};
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
+
+use persistance::{
+    answers_dao::{AnswersDao, AnswersDaoImpl},
+    questions_dao::{QuestionsDao, QuestionsDaoImpl},
 };
 
 mod handlers;
@@ -18,16 +18,30 @@ mod persistance;
 
 use handlers::*;
 
+#[derive(Clone)]
+pub struct AppState {
+    pub questions_dao: Arc<dyn QuestionsDao + Send + Sync>,
+    pub answers_dao: Arc<dyn AnswersDao + Send + Sync>,
+}
+
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
     dotenv().ok();
 
-    // let pool = PgPoolOptions::new()
-    //     .max_connections(5)
-    //     .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set"))
-    //     .await
-    //     .expect("Failed to connect to database");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+        .await
+        .expect("Failed to create Postgres connection pool!");
+
+    let questions_dao = QuestionsDaoImpl::new(pool.clone());
+    let answers_dao = AnswersDaoImpl::new(pool.clone());
+
+    let app_state = AppState {
+        questions_dao: Arc::new(questions_dao),
+        answers_dao: Arc::new(answers_dao),
+    };
 
     let app = Router::new()
         .route("/question", post(create_question))
@@ -35,7 +49,8 @@ async fn main() {
         .route("/question", delete(delete_question))
         .route("/answer", post(create_answer))
         .route("/answers", get(read_answers))
-        .route("/answer", delete(delete_answer));
+        .route("/answer", delete(delete_answer))
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
